29fc2eb5a0cd0c97a94c3d8c6bf6ce39e14b2e42
Bharat Mathe <bharatmathe@gmail.com>
2016-05-20 23:44:55 -0400

    Initial Commit

---
+++ gtest_fun/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gtest_fun
+//
+//  Created by Bharat Mathe on 5/20/16.
+//  Copyright © 2016 Bharat Mathe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
